7c3ab6583a5cf189af5f5d9746103ec9db0544b8
Niels van Velzen <git@ndat.nl>
2023-08-20 09:32:18 +0200

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,6 @@ export { Octokit } from 'npm:@octokit/rest';
 export { throttling } from 'npm:@octokit/plugin-throttling';
 export type PullRequest = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items'][number];
 
-export * as yaml from 'https://deno.land/std@0.165.0/encoding/yaml.ts';
-export * as path from 'https://deno.land/std@0.165.0/path/mod.ts';
-export * as flags from 'https://deno.land/std@0.165.0/flags/mod.ts';
+export * as yaml from 'https://deno.land/std@0.198.0/yaml/mod.ts';
+export * as path from 'https://deno.land/std@0.198.0/path/mod.ts';
+export * as flags from 'https://deno.land/std@0.198.0/flags/mod.ts';
